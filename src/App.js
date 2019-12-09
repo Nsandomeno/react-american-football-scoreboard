dde86a6945bffe9f2b97c21db8aeb734868036ab
Nicholas Sandomeno <nicholassandomeno@Nicholass-Air.home>
2019-12-09 18:48:52 -0500

    MVP partial stretch complete.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
-
+  const [down, setDown] = useState(4);
+  const [quarter, setQuarter] = useState(1);
+  
 
   return (
     <div className="container">
@@ -26,17 +28,19 @@ function App() {
   <div className="away__score">{awayScore}</div>
           </div>
         </div>
-        <BottomRow />
+        <BottomRow quarter = {quarter} down = {down} />
       </section>
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button onClick = {() => setHomeScore(homeScore + 7) } className="homeButtons__touchdown">Home Touchdown</button>
-          <button onClick = {() => setAwayScore(homeScore + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick = {() => setHomeScore(homeScore + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick = {() => setQuarter(quarter + 1)} className="HomeButtons__fieldGoal">Quarter</button>
         </div>
         <div className="awayButtons">
           <button onClick = {() => setAwayScore(awayScore + 7)} className="awayButtons__touchdown">Away Touchdown</button>
           <button onClick = {() => setAwayScore(awayScore + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick = {() => setDown(down + 1)} className="awayButtons__fieldGoal">Down</button>
         </div>
       </section>
     </div>
